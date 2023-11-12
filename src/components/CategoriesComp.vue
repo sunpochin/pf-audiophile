aa0b5822db677d6ea1d45a0950043d3c87b7dd05
pac <sunpochin@gmail.com>
2023-11-12 23:50:56 +0800

    .shop

--- src/components/CategoriesComp.vue
+++ src/components/CategoriesComp.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex-align hero-shop">
-    <div class="shop-item flex" v-for="(item, i) in categories" v-bind:key="i">
+    <div class="shop flex" v-for="(item, i) in categories" v-bind:key="i">
       <img :src="item.img" alt="category" class="shop-img" />
       <div class="shop-txt flex-center">
         <h6>{{ item.id }}</h6>
@@ -48,13 +48,14 @@ const categories = [
 <style scoped>
 .hero-shop {
   display: flex;
+
   align-items: center;
   justify-content: center;
   gap: 6rem;
   margin: 12rem auto 3rem;
   width: 100%;
 }
-.shop-item {
+.shop {
   display: flex;
   flex-direction: column;
   height: 12rem;
