a5448ef3bb87efff7f4344e7a49c2ce086bc02b5
孫柏青 <sunpochin@gmail.com>
2023-12-20 09:09:08 +0800

    feature: try adding import components to increase coverage

--- src/components/__test__/Best.spec.js
+++ src/components/__test__/Best.spec.js
@@ -1,4 +1,10 @@
 import Best from "@/components/Best.vue";
+import Footer from "@/components/Footer.vue";
+import HeroBlock from "@/components/HeroBlock.vue";
+import NavBar from "@/components/NavBar.vue";
+import CategoriesComp from "@/components/CategoriesComp.vue";
+import ProductBox from "@/components/ProductBox.vue";
+import ProductComp from "@/components/ProductComp.vue";
 import { describe, it, expect } from "vitest";
 
 describe("Best!", () => {
