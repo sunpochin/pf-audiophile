6c487fd2e8c05535cb53ca5d91b19d9b2e1f3fec
孫柏青 <sunpochin@gmail.com>
2023-12-20 09:04:54 +0800

    feature: trying to add Best coverage

---
+++ src/components/__test__/Best.spec.js
@@ -0,0 +1,16 @@
+import Best from "@/components/Best.vue";
+import { describe, it, expect } from "vitest";
+
+describe("Best!", () => {
+  it("1 + 1 should be 2", () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  // const isNumeric = (val) => {};
+  const isNumeric = (value) => !isNaN(value - parseFloat(value));
+
+  expect(isNumeric(1)).toBe(true);
+  expect(isNumeric("100")).toBe(true);
+  expect(isNumeric(null)).toBe(false);
+  expect(isNumeric(NaN)).toBe(false);
+});
